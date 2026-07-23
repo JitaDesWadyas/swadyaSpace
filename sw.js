35c388d71f9fff284709e1942501b689e47f6b76
JitaDesWadyas <167089111+JitaDesWadyas@users.noreply.github.com>
2026-07-23 16:05:15 +0200

    Cache visual China app assets

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const VERSION='china-app-v2';
-const SHELL=['./china.html','./china.css','./china-detail.js','./manifest.webmanifest','./china-icon.svg'];
+const VERSION='china-app-v4-visual';
+const SHELL=['./china.html','./china.css','./china-detail.js','./china-polish.js','./manifest.webmanifest','./china-icon.svg'];
 self.addEventListener('install',event=>{event.waitUntil(caches.open(VERSION).then(cache=>cache.addAll(SHELL)).then(()=>self.skipWaiting()))});
 self.addEventListener('activate',event=>{event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==VERSION).map(k=>caches.delete(k)))).then(()=>self.clients.claim()))});
 self.addEventListener('fetch',event=>{
